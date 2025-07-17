5adc5555004499fc3ed87cfac1c8dc49002ed02c
Derek <amir.stowe95@gmail.com>
2025-07-17 11:48:02 -0400

    react02

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 function App() {
   const todos = [
@@ -9,13 +8,18 @@ function App() {
     {id: 2, title: "take notes"},
     {id: 3, title: "code out app"},
 ]
+
   return (
 <div>
       <h1>My Todo's</h1>
+      <TodoForm />
+      <TodoList todos={todos} / >
       <ul>
             {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+
         </ul>
             </div>
+            
   )
 }
 
